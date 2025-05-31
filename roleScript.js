99015d855bc35e0828dfd34ac79e3a20f14cdc00
wizardpyy <ckonstantinos31@gmail.com>
2025-06-01 01:16:13 +0300

    Add files via upload

---
+++ roleScript.js
@@ -0,0 +1,14 @@
+var typeData = new Typed(".role", {
+    strings: [
+      "ο Wizard",
+      " Web Developer",
+      " UI-UX Designer",
+      " Backend Developer",
+      " Coder",
+      " Full Stack Developer",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+  });
